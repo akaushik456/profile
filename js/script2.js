10b67be256381d5ca4171160277dfbd41374e501
aman62001 <kaushikaman239@gmail.com>
2022-05-15 15:03:57 -0700

    give link in icons

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $('.java a').click(function(event) {
+        console.log(event)
+    $('.active').animate({
+        width:'toggle'},2000);
+    
+    });
+});
