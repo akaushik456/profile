be61bf046d3168fc99a3f1de4e8fa21219cf1be7
aman62001 <kaushikaman239@gmail.com>
2022-05-16 16:03:55 -0700

    change in full site

--- js/script2.js
+++ js/script2.js
@@ -1,8 +1,20 @@
 $( document ).ready(function() {
+    // link section
     $('.java a').click(function(event) {
         console.log(event)
     $('.active').animate({
         width:'toggle'},2000);
     
     });
+
+    // tabbing oprations
+    $( ".user-portfolio-list a" ).click(function (event){
+        $(`.tabs`).removeClass('active');
+        event.preventDefault()
+        $(".user-portfolio-list a").removeClass('active')
+        $(event.target).addClass('active')
+        var id = $(event.target).attr('href');
+        $(`.tabs${id}`).addClass('active')
+    });
+
 });
